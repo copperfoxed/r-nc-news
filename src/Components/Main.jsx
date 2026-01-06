b0d7287ddee3a09c1b570dcf73c27917909caa27
Maeve <maeve.h.carter@outlook.com>
2026-01-06 14:18:06 +0000

    Added Main.jsx contents and filled in css

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,7 +1,22 @@
 const Main = () => {
   return (
     <div className="main">
-      <button>Article</button>
+      <h2>
+        <button>Article 1</button>
+        <div className="commentbox">
+          Top Comment, This is a really long comment that will eventually get
+          cut off when it reaches the end of the box. But with even more text
+          because its not quite there yet.
+        </div>
+      </h2>
+      <h2>
+        <button>Article 2</button>
+        <div className="commentbox">Top Comment</div>
+      </h2>
+      <h2>
+        <button>Article 3</button>
+        <div className="commentbox">Top Comment</div>
+      </h2>
     </div>
   );
 };
